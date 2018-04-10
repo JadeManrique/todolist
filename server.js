9312cd6e8eac2073a17dc54fb115a5ec968377fe
Jade Manrique <jade.a.manrique@gmail.com>
2018-04-10 18:36:15 +0000

    Initial commit

--- server.js
+++ server.js
@@ -3,3 +3,17 @@ const express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// Express Routes //
+app.use('/client', express.static('client/'));
+app.use('/js', express.static('client/js'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+http.listen(8080);
+
+console.log("Server is running!");
+
+app.post('/submitNote', function (request, result) {
+    console.log(request.body);
+});
